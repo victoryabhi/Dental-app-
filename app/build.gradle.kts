24eabdb7fcb916350bf8d3889953f967e00d93d9
Veeresh <veeresh@example.com>
2026-07-27 15:58:23 +0530

    Add kotlinx-coroutines dependencies for unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,8 @@ dependencies {
     ksp(libs.androidx.room.compiler)
 
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.core)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
